a900d84509c951553e85b13852b9ca088fdd5625
Ismail Ahmedov <iahmedov@paradigmadigital.com>
2021-12-02 14:15:51 +0100

    Add test to validation that user is friend to current

--- src/test/java/tripservicekata/user/UserShould.java
+++ src/test/java/tripservicekata/user/UserShould.java
@@ -1,18 +1,34 @@
 package tripservicekata.user;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class UserShould {
 
+    public User pedro;
+    public User maria;
+
+    @BeforeEach
+    void setUp() {
+        this.pedro = new User();
+        this.maria = new User();
+    }
+
     @Test
     void return_false_when_users_are_not_friend() {
-        User pedro = new User();
-        User maria = new User();
-
         boolean actual = pedro.isFriendWith(maria);
 
         assertThat(actual).isFalse();
+    }
+
+    @Test
+    void return_true_when_users_are_friend() {
+        pedro.addFriend(maria);
+
+        boolean actual = pedro.isFriendWith(maria);
+
+        assertThat(actual).isTrue();
     }
 }
